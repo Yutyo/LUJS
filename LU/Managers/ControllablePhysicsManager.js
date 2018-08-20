97922fff3f1a49687fbe87e2146ff8a1179f6ab4
Unknown <keefauver225@gmail.com>
2018-08-19 22:08:04 -0400

    Pushing so I can work from my desktop

--- LU/Managers/ControllablePhysicsManager.js
+++ LU/Managers/ControllablePhysicsManager.js
@@ -1,21 +1,66 @@
 const GenericManager = require('./GenericManager');
 const Components = require('../Replica/Components');
 const SerializationOrder = require('../Replica/SerializationOrder');
-
+const SerializationType = require('../Replica/SerializationType');
 class ControllablePhysicsManager extends GenericManager {
     constructor(server) {
         super(server);
 
         this._data = {};
 
+        // To fix scoping issues inside of callbacks
+        let manager = this;
+
+        /**
+         * @param {Object} object
+         */
         server.eventBus.on('new-object-created', object => {
             if(object.components.hasComponent(Components.CONTROLABLE_PHYSICS_COMPONENT)) {
                 // Add data
 
-                
+                manager._data[object.ID] = {
+                    unknown1: undefined,
+                    unknown2: undefined,
+                    unknown3: undefined,
+                    unknown4: undefined,
+                    unknown5: undefined,
+                    positionData: {
+                        x: object.position.x,
+                        y: object.position.y,
+                        z: object.position.z,
+                        rx: object.rotation.x,
+                        ry: object.rotation.y,
+                        rz: object.rotation.z,
+                        rw: object.rotation.w,
+                        isPlayerOnGround: true,
+                        unknown1: false,
+                        velocity: {
+                            x: 0,
+                            y: 0,
+                            z: 0
+                        },
+                        angularVelocity: {
+                            x: 0,
+                            y: 0,
+                            z: 0
+                        },
+                    },
+                    data: false
+                };
 
                 object.addSerializer(SerializationOrder.indexOf(Components.CONTROLABLE_PHYSICS_COMPONENT), (type, stream) => {
-                    // TODO: Add code
+                    let data = manager._data[object.ID];
+                    if(type === SerializationType.CREATION) {
+                        stream.writeBit(data.unknown1 !== undefined);
+                        stream.writeBit(data.unknown2 !== undefined);
+                    }
+                    stream.writeBit(data.unknown3 !== undefined);
+                    stream.writeBit(data.unknown4 !== undefined);
+                    stream.writeBit(data.unknown5 !== undefined);
+                    stream.writeBit(data.positionData !== undefined);
+                    if(data.positionData !== undefined) {
+
+                    }
                 });
             }
         });
